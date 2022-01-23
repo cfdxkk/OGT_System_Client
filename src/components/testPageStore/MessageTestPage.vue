0892e84d96a620a922c51f2ef736a8cd6f5cdbe2
cfdxkk <b2567240058@gmail.com>
2022-01-23 23:54:24 +0800

    实现离线消息和本地历史消息回显 :love_letter:

--- src/components/testPageStore/MessageTestPage.vue
+++ src/components/testPageStore/MessageTestPage.vue
@@ -4,7 +4,8 @@
     <h2>Welcome</h2>
 
     <button @click="select">测试数据</button>
-    <button @click="connectThenGetMessage">测试IndexedDB</button>
+    <button @click="getAndSaveMessage">测试IndexedDB</button>
+    <button @click="testGetOfflineMessage">测试离线消息</button>
 
     <div id="registerPart">
       <p>here we test register</p>
@@ -61,12 +62,15 @@ export default {
   name: "MessageTestPage",
   data: () => {
     return {
-      hostAddress: "150.158.98.146:8888",
-      // hostAddress: "localhost:8888",
+      // hostAddress: "150.158.98.146:8888",
+      hostAddress: "localhost:8888",
 
       websocket: null,
 
-      messageData: {}
+      messageData: {},
+
+      db: null,
+      message: []
     }
   },
   computed: {
@@ -76,9 +80,12 @@ export default {
 
     wsURL(){ return "ws://" + this.hostAddress + "/websocket/"},
     sendMessageUrl(){ return "http://" + this.hostAddress + "/message/messagefilterandcluster"},
+
+    offlineMessageUrl(){ return "http://" + this.hostAddress + "/messagepull/getofflinemessage"}
   },
   methods: {
 
+    //注册
     register(){
 
       let username = document.getElementById('registerUserName').value
@@ -93,6 +100,7 @@ export default {
         console.log('register status', data.data)
       })
     },
+    //登录，能往cookie里存储token
     login(){
       let username = document.getElementById('loginUserName').value
       let password = document.getElementById('loginPassword').value
@@ -110,11 +118,13 @@ export default {
         console.log('cookie',document.cookie)
       })
     },
+    //测试API
     select(){
       this.Axios.get(this.selectURL).then(data => {
         console.log(data);
       })
     },
+    //链接ws服务器
     connect() {
       //判断当前浏览器是否支持WebSocket
       if('WebSocket' in window){
@@ -123,6 +133,12 @@ export default {
         let wsUsername = cookieArray[0]
         let wsUUID = cookieArray[1]
         let wsToken = cookieArray[2]
+
+        let _this = this
+        this.Axios.get(this.offlineMessageUrl+`?uuid=${wsUUID}`).then(result => {
+          console.log(result.data)
+          _this.getAndSaveMessage(result.data)
+        })
 
         let trueWsURL = `${this.wsURL}${wsUsername}/${wsUUID}/${wsToken}`
         console.log('wsurl', trueWsURL)
@@ -151,8 +167,6 @@ export default {
         alert('Not support websocket')
       }
     },
-
-
     //关闭连接
     closeWebSocket(){
       this.websocket.close();
@@ -162,7 +176,6 @@ export default {
       let message = document.getElementById('text').value;
       this.websocket.send(message);
     },
-
     //向目标用户精准发送消息
     sendMessage2User(flag){
 
@@ -223,18 +236,396 @@ export default {
         })
       }
     },
-
     //将消息显示在网页上
     setMessageInnerHTML(innerHTML) {
       console.log('——————————————————————————————————————————')
       console.log(innerHTML)
       console.log('——————————————————————————————————————————')
-      document.getElementById('wsMessage1').innerHTML = innerHTML + '<br/>';
+      document.getElementById('wsMessage1').innerHTML += innerHTML + '<br/>';
+    },
+    //请求离线消息以及indexedDB消息存储
+    getAndSaveMessage(newOfflineMessage) {
+
+      // let _this = this
+
+
+      // In the following line, you should include the prefixes of implementations you want to test.
+      let indexedDBObj = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+      // DON'T use "var indexedDB = ..." if you're not in a function.
+      // Moreover, you may need references to some window.IDB* objects:
+      window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction
+      window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+      // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+
+
+      // 检查可用性
+      if (!indexedDBObj) {
+        window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
+      }
+
+      // 打开数据库
+      let request = indexedDBObj.open("ogtMessageIDB", 2)
+
+      // 打开失败的回调
+      request.onerror = () => {
+        // Do something with request.errorCode!
+        console.log("open indexedDB ogtMessageIDB error")
+      };
+
+      // 第一次打开或者数据库版本有更新时的回调(触发此回调后仍然会触发onsuccess)
+      request.onupgradeneeded = (event) => {
+        this.db = event.target.result;
+
+        // 判断表格是否已经存在，若不存在则新建
+        if (!this.db.objectStoreNames.contains("ogtMessage")) {
+          // 建立一个对象仓库来存储我们客户的相关信息，我们选择 messages_from 作为键（key path）
+          this.db.createObjectStore("ogtMessage", { keyPath: "messages_from", autoIncrement: true });
+        }
+      };
+
+      // 打开数据库成功的回调
+      request.onsuccess = (event) => {
+        this.db = event.target.result;
+
+        // // 去服务器请求离线消息（目前是假数据）
+        // const newOfflineMessage =
+        //     [
+        //       {
+        //         'U2': [
+        //           {
+        //             // 从uuid2发来的一条消息
+        //             message_id: '1',
+        //             chat_message_no: '0',
+        //             message_from: '2',
+        //             message_target: '1',
+        //             lang_massage_flag: '0',
+        //             message_type: 'aa',
+        //             sent_flag: '1',
+        //             read_flag: '0',
+        //             violation_flag: '0',
+        //             message: 'hello22',
+        //             sent_datetime: '0',
+        //             edit_datetime: '0',
+        //             edit_user_id: '2'
+        //           },{
+        //             // 从uuid2发来的二条消息
+        //             message_id: '1',
+        //             chat_message_no: '1',
+        //             message_from: '2',
+        //             message_target: '1',
+        //             lang_massage_flag: '0',
+        //             message_type: 'aa',
+        //             sent_flag: '1',
+        //             read_flag: '0',
+        //             violation_flag: '0',
+        //             message: '你好',
+        //             sent_datetime: '0',
+        //             edit_datetime: '0',
+        //             edit_user_id: '2'
+        //           }
+        //         ]
+        //       },{
+        //       'U3': [
+        //         {
+        //           // 从uuid3发来的一条消息
+        //           message_id: '1',
+        //           chat_message_no: '0',
+        //           message_from: '2',
+        //           message_target: '1',
+        //           lang_massage_flag: '0',
+        //           message_type: 'aa',
+        //           sent_flag: '1',
+        //           read_flag: '0',
+        //           violation_flag: '0',
+        //           message: 'test01',
+        //           sent_datetime: '0',
+        //           edit_datetime: '0',
+        //           edit_user_id: '2'
+        //         },{
+        //           // 从uuid3发来的二条消息
+        //           message_id: '1',
+        //           chat_message_no: '1',
+        //           message_from: '2',
+        //           message_target: '1',
+        //           lang_massage_flag: '0',
+        //           message_type: 'aa',
+        //           sent_flag: '1',
+        //           read_flag: '0',
+        //           violation_flag: '0',
+        //           message: 'test02',
+        //           sent_datetime: '0',
+        //           edit_datetime: '0',
+        //           edit_user_id: '2'
+        //         }
+        //       ]
+        //     }
+        //     ]
+
+
+        // // 去服务器请求离线消息（目前是假数据）
+        // const newOfflineMessage =
+        //     [
+        //       {
+        //         'U2': [
+        //           {
+        //             // 从uuid2发来的一条消息
+        //             message_id: '1',
+        //             chat_message_no: '0',
+        //             message_from: '2',
+        //             message_target: '1',
+        //             lang_massage_flag: '0',
+        //             message_type: 'aa',
+        //             sent_flag: '1',
+        //             read_flag: '0',
+        //             violation_flag: '0',
+        //             message: 'hello04',
+        //             sent_datetime: '0',
+        //             edit_datetime: '0',
+        //             edit_user_id: '2'
+        //           }
+        //         ]
+        //       },{
+        //         'U3': [
+        //         {
+        //           // 从uuid3发来的一条消息
+        //           message_id: '1',
+        //           chat_message_no: '0',
+        //           message_from: '2',
+        //           message_target: '1',
+        //           lang_massage_flag: '0',
+        //           message_type: 'aa',
+        //           sent_flag: '1',
+        //           read_flag: '0',
+        //           violation_flag: '0',
+        //           message: 'test04',
+        //           sent_datetime: '0',
+        //           edit_datetime: '0',
+        //           edit_user_id: '2'
+        //         }
+        //       ]
+        //     }
+        //     ]
+
+        // const newOfflineMessage = [
+        //   {
+        //     "messageId": null,
+        //     "chatMessageNo": 2,
+        //     "messageFrom": "b41849ea1e824ae89f0c679089f48899",
+        //     "messageTarget": "a8dd8605ef5745cf96d2096f5613cba1",
+        //     "longMessageFlag": 0,
+        //     "messageType": "aa",
+        //     "sendFlag": 0,
+        //     "readFlag": 0,
+        //     "violationFlag": 0,
+        //     "message": "offline01",
+        //     "sentDate": 1642941685313,
+        //     "editDate": null,
+        //     "messageEditorID": null
+        //   },
+        //   {
+        //     "messageId": null,
+        //     "chatMessageNo": 3,
+        //     "messageFrom": "b41849ea1e824ae89f0c679089f48899",
+        //     "messageTarget": "a8dd8605ef5745cf96d2096f5613cba1",
+        //     "longMessageFlag": 0,
+        //     "messageType": "aa",
+        //     "sendFlag": 0,
+        //     "readFlag": 0,
+        //     "violationFlag": 0,
+        //     "message": "offline02",
+        //     "sentDate": 1642941689672,
+        //     "editDate": null,
+        //     "messageEditorID": null
+        //   },
+        //   {
+        //     "messageId": null,
+        //     "chatMessageNo": 4,
+        //     "messageFrom": "b41849ea1e824ae89f0c679089f48899",
+        //     "messageTarget": "a8dd8605ef5745cf96d2096f5613cba1",
+        //     "longMessageFlag": 0,
+        //     "messageType": "aa",
+        //     "sendFlag": 0,
+        //     "readFlag": 0,
+        //     "violationFlag": 0,
+        //     "message": "offline03",
+        //     "sentDate": 1642941694633,
+        //     "editDate": null,
+        //     "messageEditorID": null
+        //   },
+        //   {
+        //     "messageId": null,
+        //     "chatMessageNo": 5,
+        //     "messageFrom": "b41849ea1e824ae89f0c679089f48899",
+        //     "messageTarget": "a8dd8605ef5745cf96d2096f5613cba1",
+        //     "longMessageFlag": 1,
+        //     "messageType": "aa",
+        //     "sendFlag": 0,
+        //     "readFlag": 0,
+        //     "violationFlag": 0,
+        //     "message": "一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五...",
+        //     "sentDate": 1642943513590,
+        //     "editDate": null,
+        //     "messageEditorID": null
+        //   }
+        // ]
+
+        let dataArr = [];
+        newOfflineMessage.map(mapItem => {
+          if (dataArr.length == 0) {
+            dataArr.push({  [mapItem.messageFrom]: [mapItem] })
+          } else {
+            let res = dataArr.some(item=> {//判断相同日期，有就添加到当前项
+              if (mapItem.messageFrom === Object.keys(item)[0]) {
+                console.log("same",item)
+                item[Object.keys(item)[0]].push(mapItem)
+                return true
+              }
+            })
+            if (!res) {//如果没找相同日期添加一个新对象
+              dataArr.push({ [mapItem.messageFrom]: [mapItem] })
+            }
+          }
+        })
+        console.log('dataArr is wtf?',dataArr)
+
+
+
+
+        // 创建一个事务，类型：IDBTransaction
+        let transaction = this.db.transaction(["ogtMessage"], 'readwrite')
+        // 通过事务来获取IDBObjectStore
+        let store = transaction.objectStore("ogtMessage")
+        // 从idb中获得所有数据
+        let allRecordsStore = store.getAll()
+        // 获取数据成功的回调
+        allRecordsStore.onsuccess = () => {
+          // 取得数据
+          let idbMessage = allRecordsStore.result.slice(-50)
+          console.log("idbMessage-------------",idbMessage)
+
+          let mergeMessage = []  // 最后整理后的消息数组
+
+          // 验证是否从idb取到值了(因为idb里有可能是空的)
+          if(idbMessage == null || idbMessage.length === 0){
+            console.log('没取到')
+            // 没取到就把遍历取到的离线消息数据并插入到idb中
+            dataArr.forEach(messages => {
+              // 转换整理成适合idb存储的格式
+              let IndexedDBRightMessage = {
+                // 根据messages_from分组
+                'messages_from' : Object.keys(messages)[0],
+                // 截取当前messages_from来源发送来的后50条消息
+                'messages': Object.values(messages)[0].slice(-50)
+              }
+              console.log("before add is ",IndexedDBRightMessage)
+              // 存储到Idb
+              store.add(IndexedDBRightMessage)
+              // 把转换后的消息插入到整理后的消息数组中
+              mergeMessage.push(IndexedDBRightMessage)
+              IndexedDBRightMessage.messages.forEach(allMessage => {
+                document.getElementById('wsMessage1').innerHTML += `收到来自 ${IndexedDBRightMessage.messages_from} 的离线消息: ${allMessage.message} ` + `<br/>`;
+              })
+
+            })
+            // 把整理后的消息显示在桌面上(实际上不会这么做，只有点开一个聊天时才会触发这个)
+            // _this.showIDBMessage2Page(mergeMessage)
+          } else {  // 从idb中成功得到了数据
+            //把新离线消息插入到idb中
+            dataArr.forEach(messages => {
+              // 获取新离线消息中的消息from
+              let messagesFrom = Object.keys(messages)[0]
+              console.log("messagesFrom",messagesFrom)
+              let offlineMessages = Object.values(messages)[0]
+              console.log('offlineMessages',offlineMessages)
+              // 在idb中根据from寻找
+              let messageObjSearchFromMessageFrom = store.get(messagesFrom)
+              messageObjSearchFromMessageFrom.onsuccess = () => {
+                let messageObjSearchFromMessageFromResult = messageObjSearchFromMessageFrom.result
+                if(messageObjSearchFromMessageFromResult !== undefined || messageObjSearchFromMessageFromResult !== null){
+                  // 如果找到了
+                  console.log("找到了")
+                  // 根据上面根据message_from找到的message对象得到messages
+                  let messageSearchFromMessageFrom = messageObjSearchFromMessageFromResult.messages
+                  console.log("??????????????messageObjSearchFromMessageFrom is: ",messageObjSearchFromMessageFrom)
+                  // 把新离线消息插入到idb消息中
+                  offlineMessages.forEach(offlineMessage => {
+                    messageSearchFromMessageFrom.push(offlineMessage)
+                  })
+                  // 转换整理成适合idb存储的格式
+                  let IndexedDBRightMessage = {
+                    // 根据messages_from分组
+                    'messages_from' : messagesFrom,
+                    // 截取后50条消息
+                    'messages': messageSearchFromMessageFrom.slice(-50)
+                  }
+                  console.log("before update, date is:",IndexedDBRightMessage)
+                  // 插入回idb
+                  store.put(IndexedDBRightMessage)
+                  // 把转换后的消息插入到整理后的消息数组中
+                  mergeMessage.push(IndexedDBRightMessage)
+
+                  IndexedDBRightMessage.messages.forEach(allMessage => {
+                    document.getElementById('wsMessage1').innerHTML += `收到来自 ${IndexedDBRightMessage.messages_from} 的离线消息: ${allMessage.message} ` + `<br/>`;
+                  })
+
+                } else { // 如果没找到
+                  // 转换整理成适合idb存储的格式
+                  let IndexedDBRightMessage = {
+                    // 根据messages_from分组
+                    'messages_from' : messagesFrom,
+                    // 截取后50条消息
+                    'messages': offlineMessages.slice(-50)
+                  }
+                  // 插入回idb
+                  store.put(IndexedDBRightMessage)
+                  // 把转换后的消息插入到整理后的消息数组中
+                  mergeMessage.push(IndexedDBRightMessage)
+                  IndexedDBRightMessage.messages.forEach(allMessage => {
+                    document.getElementById('wsMessage1').innerHTML += `收到来自 ${IndexedDBRightMessage.messages_from} 的离线消息: ${allMessage.message} ` + `<br/>`;
+                  })
+                }
+              }
+
+            })
+            // _this.showIDBMessage2Page(mergeMessage)
+          }
+        }
+      }
+    },
+    //整理并展示获取的离线消息
+    showIDBMessage2Page(message){
+
+      console.log(message)
+
+      let _this = this
+      // alert(message)
+      //   alert('aa')
+        message.forEach(mess => {
+          _this.setMessageInnerHTML("在您离线时，您收到了来自" + mess.messages_from + `的如下消息:</br>`)
+          mess.messages.forEach(realMassage => {
+            _this.setMessageInnerHTML(realMassage + `</br>`)
+          })
+        })
+
+    },
+    //测试获取离线消息
+    testGetOfflineMessage() {
+
+      let cookieArray = (document.cookie.split('=')[1]).split('-');
+      let uuid = cookieArray[1]
+
+      this.Axios.get(this.offlineMessageUrl+`?uuid=${uuid}`).then(result => {
+        console.log(result.data())
+        result.data().forEach(message => {
+          console.log(message.message);
+        })
+      })
+      console.log("testGetOfflineMessage end")
     }
   },
   mounted() {
     //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-    window.onbeforeunload = function(){
+    window.onbeforeunload = () => {
       this.websocket.close();
     }
   }
